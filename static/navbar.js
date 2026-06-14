4ab9826c8de5b1d0163edde0c603ea077f08f8b3
leen2233 <arslanrejepow223@gmail.com>
2026-06-14 20:33:44 +0800

    made navbar active items working again

--- static/navbar.js
+++ static/navbar.js
@@ -1,5 +1,6 @@
 function getAndChange(url) {
   window.history.pushState({}, "", url);
+  makeCurrentNavActive();
 
   fetch(url, {
       method: "GET",
@@ -12,5 +13,48 @@ function getAndChange(url) {
       document.querySelector("#content").innerHTML = html;
       document.dispatchEvent(new Event("content:loaded"))
     })
+}
+
+
+document.addEventListener("DOMContentLoaded", makeCurrentNavActive)
+
+
+function makeCurrentNavActive () {
+  // Set current navbar group  active
+  let path = window.location.pathname;
+  
+  let groups = document.querySelectorAll(".navbar-group")
+  groups.forEach(element => element.classList.remove("active"))
+
+  if (path == "/" || path == "/add" || path == "/discover" || path.startsWith("/comic/")) {
+    document.getElementById("home").classList.add("active")
+  }
+  if (path == "/activity") {
+    document.getElementById("activity").classList.add("active")
+  }
+  if (path == "/settings") {
+    document.getElementById("settings").classList.add("active")
+  }
+
+  // Set current navbar item active
+  let items = document.querySelectorAll(".navbar-item")
+  items.forEach(element => element.classList.remove("active"))
+
+  if (path == "/") {
+    document.getElementById("home-home").classList.add("active")
+  }
+  if (path == "/add") {
+    document.getElementById("home-add-new").classList.add("active")
+  }
+  if (path == "/discover") {
+    document.getElementById("home-discover").classList.add("active")
+  }
+  if (path == "/activity") {
+    document.getElementById("activity-activity").classList.add("active")
+  }
+  if (path == "/settings") {
+    document.getElementById("settings-settings").classList.add("active")
+  }
+
 }
 
